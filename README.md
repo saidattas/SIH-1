fb4a9617005d201b3c921fd93e66b1faf59f39dc
saidatta1 <b118051@iiit-bh.ac.in>
2020-01-13 21:11:19 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# SIH
+# SIH-Website
---
+++ index.js
@@ -0,0 +1,21 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+app.listen(3000, () => {
+	console.log('SERVER IS RUNNING!');
+});
